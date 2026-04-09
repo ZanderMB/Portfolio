d858adbe09d6a4595499bca87eb2994123dff85f
ZanderMB <zandermbroome2005@outlook.com>
2026-04-08 23:08:42 -0300

    disable some dev stuff

--- src/panels/home.js
+++ src/panels/home.js
@@ -7,7 +7,7 @@ const homePanel = {
         "I build web apps, containerized services via Docker, and bare-metal GNS3 network simulators. I'm passionate about building clean efficient apps, building apps people actually have need of, and understanding the full stack from the web to the infrastructure."
     ],
     homeCTA: "Some of my projects can be found here.",
-    aside: "src/assets/Glorpo.webp"
+    aside: headshot
 };
 
 export default homePanel;
