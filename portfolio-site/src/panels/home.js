79803413723f71e1d114eab2b3626c0d8865c1ec
ZanderMB <zandermbroome2005@outlook.com>
2026-04-09 08:29:47 -0300

    minor changes

--- portfolio-site/src/panels/home.js
+++ portfolio-site/src/panels/home.js
@@ -1,5 +1,5 @@
 const homePanel = {
-    title: "Zander's Portfolio",
+    title: "Zander Broome",
     role: "Web/Software Developer & Aspiring Cybersecurity Specialist",
     description: [
         "I build web apps, containerized services via Docker, and bare-metal GNS3 network simulators. I'm passionate about building clean efficient apps, building apps people actually have need of, and understanding the full stack from the web to the infrastructure."
